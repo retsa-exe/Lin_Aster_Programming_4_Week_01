5c3b131783706e9ab297f3c69840dae1e3da4121
lin62 <lin62@OA-S141-25>
2026-01-07 17:26:44 -0500

    Add project files.

---
+++ Assets/Scripts/DisableAT.cs
@@ -0,0 +1,44 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class DisableAT : ActionTask {
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			Debug.Log("Initializing.");
+
+
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+			Debug.Log("Executing.");
+
+			agent.gameObject.SetActive(false);
+			//EndAction(true);
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			Debug.Log("Updating.");
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
